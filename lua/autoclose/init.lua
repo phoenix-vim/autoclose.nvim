cdc875dabec47a2e67e94c5db4b5adadff6d7ef5
inadati <dr.inadati@gmail.com>
2022-09-08 19:56:50 +0900

    囲いを削除したときの処理をコメントアウト

--- lua/autoclose/init.lua
+++ lua/autoclose/init.lua
@@ -12,21 +12,21 @@ M.close = function()
     --map("i", "\"", "\"\"<ESC>i", {noremap = true})
 end
 
-M.deleteAndIndent = function()
-    local col = vim.fn.getpos(".")[3]
-
-    local cursor_line = vim.api.nvim_get_current_line()
-    local cursor_left = string.sub(cursor_line, col - 1, col - 1)
-    local cursor_position = string.sub(cursor_line, col, col)
-
-    if modules.delete(cursor_left) and cursor_position == modules.pair(cursor_left) then
-	map("i", "<BS>", "<ESC>\"_2s", {noremap = true})
-	map("i", "<Enter>", "<Enter><ESC>O", {noremap = true})
-    else
-	map("i", "<BS>", "<BS>", {noremap = true})
-	map("i", "<Enter>", "<Enter>", {noremap = true})
-    end
-end
+-- M.deleteAndIndent = function()
+--     local col = vim.fn.getpos(".")[3]
+--
+--     local cursor_line = vim.api.nvim_get_current_line()
+--     local cursor_left = string.sub(cursor_line, col - 1, col - 1)
+--     local cursor_position = string.sub(cursor_line, col, col)
+--
+--     if modules.delete(cursor_left) and cursor_position == modules.pair(cursor_left) then
+-- 	map("i", "<BS>", "<ESC>\"_2s", {noremap = true})
+-- 	map("i", "<Enter>", "<Enter><ESC>O", {noremap = true})
+--     else
+-- 	map("i", "<BS>", "<BS>", {noremap = true})
+-- 	map("i", "<Enter>", "<Enter>", {noremap = true})
+--     end
+-- end
 
 M.escape = function()
     local col = vim.fn.getpos(".")[3]
